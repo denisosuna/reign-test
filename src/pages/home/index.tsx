d7bbf33b8a6e7c163c61089ee7562f1ecc9dd9f4
denisosuna <denisosuna@gmail.com>
2022-02-24 08:59:26 -0400

    try ci

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import Card from "../../components/card";
 import "./home.css";
 
 const home = () => {
-  return <div className="main"><Card author="author" story_title="story_title" story_url="story_url" created_at="created_at" /></div>;
+  return <><div className="main"><Card author="author" story_title="story_title" story_url="story_url" created_at="created_at" /></div></>;
 };
 
 export default home;
