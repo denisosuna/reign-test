b020f53860a25b42425422aba673f2380ff93e37
denisosuna <denisosuna@gmail.com>
2022-02-24 09:24:06 -0400

    fix: tests

--- src/components/icons/WatchIcon/index.tsx
+++ src/components/icons/WatchIcon/index.tsx
@@ -15,15 +15,15 @@ const WatchIcon: React.FC<{ cls?: string }> = ({ cls }) => (
     <g
       id="Page-1"
       stroke="none"
-      stroke-width="1"
+      strokeWidth="1"
       fill="none"
-      fill-rule="evenodd"
+      fillRule="evenodd"
     >
       <g
         id="Front-End-Test---Home-view"
         transform="translate(-766.000000, -362.000000)"
         fill="#606060"
-        fill-rule="nonzero"
+        fillRule="nonzero"
       >
         <g id="News" transform="translate(740.000000, 348.000000)">
           <g id="iconmonstr-time-2" transform="translate(26.000000, 14.000000)">
